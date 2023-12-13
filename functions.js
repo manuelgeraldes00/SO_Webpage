0c2ac881075147662487fbff4908f88fbd1f3628
Manuel Geraldes <manuelgeraldes2000@gmail.com>
2023-12-13 11:05:13 +0000

    Improve button functionality

--- functions.js
+++ functions.js
@@ -1,3 +1,11 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+function showMsg() {
+    document.getElementById("demo").innerHTML = "Hello World!";
+    sleep(1000).then(() => {
+        document.getElementById("demo").innerHTML = "Bye World!"; });
+    sleep(2000).then(() => {
+        document.getElementById("demo").innerHTML = ""; });
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
